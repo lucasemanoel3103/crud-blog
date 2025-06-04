2cfa6cfb5224e25d5312147f10e367b01e9c944c
Lucas emanoel <lucasemanoel625@gmail.com>
2025-06-03 21:52:12 -0300

    refactor: Reorganização do código usando controller e feat: Criação das funcionalidades em categoria e artigos do blog.

--- database/database.js
+++ database/database.js
@@ -1,8 +1,13 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('crudblog', 'root', '3103Lucas@',{
-    host: 'localhost',
-    dialect: 'mysql'
+const connection = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
 });
 
 module.exports = connection;
