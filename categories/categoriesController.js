4cc941f2d1f4589d8466963ed2507daa785af39a
Lucas emanoel <lucasemanoel625@gmail.com>
2025-06-10 19:11:51 -0300

    Implementação do método para deletar categorias cadastradas no banco de dados

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("./Category");
-const slugify = require("slugify")
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) => {
   res.render("admin/categories/new");
@@ -10,22 +10,40 @@ router.get("/admin/categories/new", (req, res) => {
 router.post("/categories/save", (req, res) => {
   let title = req.body.title;
   if (title != undefined) {
-      Category.create({
-        title: title,
-        slug: slugify(title)
-      }).then(() => {
-        res.redirect("/");
-      })
-    
+    Category.create({
+      title: title,
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect("/");
+    });
   } else {
     res.redirect("/admin/categories/new");
   }
 });
 
 router.get("/admin/categories", (req, res) => {
-    Category.findAll().then(categories => {
-       res.render("admin/categories/index", {categories: categories});  
-    });
+  Category.findAll().then((categories) => {
+    res.render("admin/categories/index", { categories: categories });
+  });
+});
+
+router.post("/categories/delete", (req, res) => {
+  let id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/categories");
+      });
+    } else {
+      res.redirect("/admin/categories");
+    }
+  } else {
+    res.redirect("/admin/categories");
+  }
 });
 
 module.exports = router;
