777daefc86b8d42644773d78680529adc407f13e
Lucas emanoel <lucasemanoel625@gmail.com>
2025-06-11 20:08:05 -0300

    feat: Implementação do método para edição de uma categoria

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -14,7 +14,7 @@ router.post("/categories/save", (req, res) => {
       title: title,
       slug: slugify(title),
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/categories");
     });
   } else {
     res.redirect("/admin/categories/new");
@@ -45,5 +45,23 @@ router.post("/categories/delete", (req, res) => {
     res.redirect("/admin/categories");
   }
 });
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+      let id = req.params.id;
+
+      if(isNaN(id)){
+        return res.redirect("/admin/categories");
+      }
+
+      Category.findByPk(id).then(category => {
+          if(category != undefined){
+            res.render("admin/categories/edit",{category: category});
+          }else{
+            res.redirect("/admin/categories");
+          }
+      }).catch(erro => {
+        res.redirect("/admin/categories")
+      })
+})
 
 module.exports = router;
