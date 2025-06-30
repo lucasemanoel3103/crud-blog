50174721f0007819b7c656d3b60efc5eb5025d1b
Lucas emanoel <lucasemanoel625@gmail.com>
2025-06-30 19:50:50 -0300

    chore: Ajuste de tempo no sequelize

--- database/database.js
+++ database/database.js
@@ -7,7 +7,8 @@ const connection = new Sequelize(
     process.env.DB_PASS,
     {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    timezone: "-03:00"
 });
 
 module.exports = connection;
