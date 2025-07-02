7b147e9dd221e824359a10bf22b13106759c9451
Lucas emanoel <lucasemanoel625@gmail.com>
2025-07-02 19:31:13 -0300

    feat:   Implementação da rota para filtra artigos por categoria

--- index.js
+++ index.js
@@ -62,6 +62,28 @@ app.get("/:slug", (req, res) => {
     })
 })
 
+app.get("/category/:slug",(req, res) => {
+    let slug = req.params.slug;
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if(category != undefined){
+
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            })
+
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err => {
+        res.redirect("/");
+    })
+})
+
 app.listen(3000, () =>{
     console.log("Servidor rodando na porta 3000!");
 });
